0a9f8390604e9d9cf09611ac4e56ba134e403488
Eldar Zainashev <aeoloth@yandex.ru>
2021-09-04 00:48:11 +0300

    add changing days and stake

--- hw-4-t6/src/App.js
+++ hw-4-t6/src/App.js
@@ -1,21 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function Table(props) {
+  const [days, setDays] = useState(props.worker.days);
+  const [stake, setStake] = useState(props.worker.stake);
+
   return (
-      <tr>
-        <td>{props.worker.name}</td>
-        <td>{props.worker.lastname}</td>
-        <td>
-          <input type="text" value={props.worker.days} onChange={props.onChangeDays} />
-        </td>
-        <td>
-        <input type="text" value={props.worker.stake} onChange={props.onChangeStake} />
-        </td>
-        <td>
-          {props.worker.days * props.worker.stake}
-        </td>
-      </tr>
+    <tr>
+      <td>{props.worker.name}</td>
+      <td>{props.worker.lastname}</td>
+      <td>
+        <input
+          type="text"
+          value={days}
+          onChange={(e) => setDays(e.target.value)}
+        />
+      </td>
+      <td>
+        <input
+          type="text"
+          value={stake}
+          onChange={(e) => setStake(e.target.value)}
+        />
+      </td>
+      <td>{days * stake}</td>
+    </tr>
   );
 }
 
@@ -35,73 +44,34 @@ class App extends React.Component {
     };
   }
 
-  handleDays(name, lastname) {
-    const workers = this.state.workers.concat()
-
-    const worker = workers.find(c => c.name === name & c.lastname === lastname)
-    worker.days = 1
-
-    this.setState({worker})
-  }
-
-  handleStake(name, lastname) {
-    const workers = this.state.workers.concat()
-
-    const worker = workers.find(c => c.name === name & c.lastname === lastname)
-    worker.stake = 3
-
-    this.setState({worker})
-  }
-
   renderTable() {
     return this.state.workers.map((worker) => {
       return (
         <Table
           worker={worker}
           key={worker.name + worker.lastname + Math.random()}
-          onChangeDays={this.handleDays.bind(this, worker.name, worker.lastname)}
-          onChangeStake={this.handleStake.bind(this, worker.name, worker.lastname)}
         />
       );
     });
   }
 
-//   renderTable() {
-//   return( this.state.workers.map((worker) => {
-//     return (
-//     <tr>
-//         <td>{worker.name}</td>
-//         <td>{worker.lastname}</td>
-//         <td>
-//           <input type="text" value={worker.days}  onChange/>
-//         </td>
-//         <td>
-//           {worker.stake}
-//         </td>
-//         <td>
-//           {worker.days * worker.stake}
-//         </td>
-//       </tr>
-//         )
-// } ) )}
-
   render() {
     return (
-    <div><table>
-      <thead>
-        <tr>
-          <td>Имя</td>
-          <td>Фамилия</td>
-          <td>Отработано дней</td>
-          <td>Ставка в день, руб.</td>
-          <td>Заработная плата, руб.</td>
-        </tr>
-      </thead>
-      <tbody>
-          {this.renderTable()}
-      </tbody>
-    </table></div>
-    )
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <td>Имя</td>
+              <td>Фамилия</td>
+              <td>Отработано дней</td>
+              <td>Ставка в день, руб.</td>
+              <td>Заработная плата, руб.</td>
+            </tr>
+          </thead>
+          <tbody>{this.renderTable()}</tbody>
+        </table>
+      </div>
+    );
   }
 }
 
